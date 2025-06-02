3f85e1f3e2de92904579d46223421cadfab6352d
athararyasatya <133422563+athararyasatya@users.noreply.github.com>
2025-06-03 04:20:57 +0700

    nambah halaman register login

--- js/script.js
+++ js/script.js
@@ -24,3 +24,15 @@ document.addEventListener("click", function (event) {
     offcanvasInstance.hide();
   }
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const togglePassword = document.getElementById('togglePassword');
+  const passwordInput = document.getElementById('password');
+  const eyeIcon = document.getElementById('eyeIcon');
+
+  togglePassword.addEventListener('click', () => {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    eyeIcon.classList.toggle('bi-eye');
+    eyeIcon.classList.toggle('bi-eye-slash');
+  });
+});
